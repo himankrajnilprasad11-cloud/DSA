4559dd05da20c9eb1c39ac93a1ab439e3c0a6497
himankrajnilprasad11-cloud <himankrajnilprasad11@gmail.com>
2026-07-01 18:47:48 +0530

    Time: 2 ms (15.84%) | Memory: 9.7 MB (63.43%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.cpp
+++ 118-pascals-triangle/pascals-triangle.cpp
@@ -1,13 +1,11 @@
 class Solution {
 public:
-    std::vector<std::vector<int>> generate(int numRows) {
-        std::vector<std::vector<int>> triangle(numRows);
+    vector<vector<int>> generate(int numRows) {
+        vector<vector<int>> triangle(numRows);
 
         for (int i = 0; i < numRows; ++i) {
-            // Resize the current row to hold i + 1 elements and default them to 1
             triangle[i].resize(i + 1, 1);
             
-            // Fill in the interior elements of the row
             for (int j = 1; j < i; ++j) {
                 triangle[i][j] = triangle[i - 1][j - 1] + triangle[i - 1][j];
             }
